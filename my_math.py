f9e82a0e2c2ac71e45e8bd62188fd9dca65a9092
thirupathi-rao <dtrrises@gmail.com>
2025-02-08 01:19:47 +0530

    other

---
+++ my_math.py
@@ -0,0 +1,11 @@
+#%%writefile should be the first line of the cell
+#creating fuction in the file created above
+def add_(a,b):
+    return a+b
+def sub_(a,b):
+    return a-b
+def mult_(a,b):
+    return a*b
+def divide_(a,b):
+    return a/b
+    
